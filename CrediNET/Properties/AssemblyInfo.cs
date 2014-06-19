dcda3b3fd4936958be133848ad9426664ac3f170
zdimension <zippedfire@free.fr>
2014-06-19 08:07:02 +0200

    Some AssemblyInfo stuff

--- CrediNET/Properties/AssemblyInfo.cs
+++ CrediNET/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.8.0.0")]
 [assembly: AssemblyFileVersion("1.8.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("fr-FR")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
